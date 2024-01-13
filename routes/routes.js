66e08822f25366c9bca7d908980185f7e3adbf7c
Josh <55717114+jmccann897@users.noreply.github.com>
2024-01-13 13:16:45 +0000

    Refactor dash.ejs
    Refactored the dash ejs to show the results from the database per snapshot ID.

--- routes/routes.js
+++ routes/routes.js
@@ -5,17 +5,26 @@ const conn = require('../utils/dbconn');
 //creating router object
 const router = express.Router();
 
+router.use(express.json()); //for parsing app/json
+router.use(express.urlencoded({extended: true}));
+
 //establishing path
 const viewdir = path.join(__dirname,'..', 'ejs');
 
 //Routing - should be in own file structure
 //route handler for http GET requests for default / paths
 router.get("/", (req, res) => {
-  res.status(200);
-  //If using static html - use sendFile to rel path
-    res.send('<h1>Welcome page</h1>')
-  //res.render("welcome");
+  const SQLquery = 'SELECT * FROM  snapshot INNER JOIN snapshot_emotion ON snapshot.snapshot_id = snapshot_emotion.snapshot_id';
+  conn.query(SQLquery, (err, rows) =>{
+    if(err){
+      throw err;
+    } else{
+      res.render('mytemplate', {history:rows});
+    };
+  });
 });
+
+
 
 //trying to add new path for form submission
 router.get("/form", (req, res) => {
@@ -36,18 +45,16 @@ router.post("/login", (req, res) =>{
 //route for dashboard
 router.get("/dash", (req, res) => {
   //adding in sql reading -- NEED TO DO A JOIN QUERY TO GET DATES AND SCORES
-  const selectSQL = 'SELECT * FROM  snapshot INNER JOIN snapshot_emotion ON snapshot.snapshot_id = snapshot_emotion.snapshot_id';
-  //res.render('dash');
-  
+  const selectSQL = 'SELECT * FROM  snapshot INNER JOIN snapshot_emotion ON snapshot.snapshot_id = snapshot_emotion.snapshot_id'; 
   conn.query(selectSQL, (err, rows) =>{
     if(err) {
       throw err;
-    } else{
-      console.log(rows);
+    } else {
       res.render('dash', {history: rows});
-    }
-  });
+    };
 });
+});
+
 
 //route for signup
 router.get("/signup", (req, res) => {
@@ -59,7 +66,8 @@ router.get("/signup", (req, res) => {
 router.get("/addsnap", (req, res) =>{
   res.status(200);
   res.render("addsnap");
-})
+});
+
 //route for post form sub in addsnap
 router.post("/addsnap", (req, res) => {
   const data = req.body;
