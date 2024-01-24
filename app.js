749e18fd81ff7bdc9180e59325cc9f179f7f40f7
Josh <55717114+jmccann897@users.noreply.github.com>
2024-01-24 19:01:56 +0000

    Add updates from Leo
    Refactored app.js and routes.js to have const, app.use then routes ordering in app.js, removed app.use methods in routes. Then added route for form action in editsnap.ejs

--- app.js
+++ app.js
@@ -8,43 +8,27 @@ const path = require('path');
 
 //creating express object rep our app
 const app = express(); 
-
+//provides minimal http req info logging
+app.use(morgan("tiny"));
 /*To dynamically render html, need to use engine
 To use engine, it needs set on app object*/
 app.set('view engine', 'ejs');
-
 /*setting the directory for static files in public folder 
 via express' static method
 Use method allows mounting to app object*/
 app.use(express.static(__dirname+ '/public'));
-
-//Need to import node file system module
-const fs = require('fs');
-//Create data var and parse the data file containing JSON
-const data = JSON.parse(
-    fs.readFileSync(__dirname + '/data/students.json')
-);
-
-//provides minimal http req info logging
-app.use(morgan("tiny"));
-
-//db connection
-
 /*parser for incoming requests with URL encoding
 extended allows for arrays eg JSON arrays*/
 app.use(express.json()); //for parsing app/json
 app.use(express.urlencoded({extended: true}));
-
 //initialising session middleware - prior to router
 app.use(session({
   secret:'mysecretstring1234', //used to sign hash that is stored as session id in the cookie passed to client
   resave: false, // indicates session won't save on every req made
   saveUninitialized: false // indicates no session saved for a req that doesn't need to save sessions
 }));
-
 //routing --> handled in routes.js
 app.use('/', router);
-
 //Binding the app object to server on specified port
 app.listen(process.env.PORT, (err) => {
   if (err) return console.log(err);
--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,11 @@
 const express = require("express");
-const { stat } = require("fs");
 const path = require("path");
 const conn = require('../utils/dbconn');
 const controller = require('./../controllers/controllers');
 //creating router object
 const router = express.Router();
-
-router.use(express.json()); //for parsing app/json
-router.use(express.urlencoded({extended: true}));
-
 //establishing path
 const viewdir = path.join(__dirname,'..', 'ejs');
-
 //get routes
 router.get('/', controller.getDefault);
 router.get('/edit/:id', controller.getEdit);
@@ -27,6 +21,5 @@ router.post('/edit/:id', controller.postEdit);
 router.post('/del/:id', controller.postDelete);
 router.post('/addsnap', controller.postAddsnap);
 router.post('/login', controller.postLogin);
-
 
 module.exports = router;
